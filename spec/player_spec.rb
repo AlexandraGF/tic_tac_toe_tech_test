fb0b7182499532d04a9847b761440b2620a266a1
Alexy <alexandra.fina@yahoo.com>
2018-03-01 11:29:36 +0000

    Create spec for player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+# require './lib/player.rb'
+
+describe Player do
+
+  describe 'Player creation: ' do
+    it 'check that class can create player with name' do
+      player = Player.new("Menah")
+      expect(player.name).to eq("Menah")
+    end
+  end
+end
