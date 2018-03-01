a0e0ba9dcff89ca58b9af3965917b459a4660036
Alexy <alexandra.fina@yahoo.com>
2018-03-01 11:53:18 +0000

    Test the option and position from the player's turn are added to the correct array

--- lib/tic.rb
+++ lib/tic.rb
@@ -1,13 +1,17 @@
 class TicTacToe
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :tic_array, :positions_array
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @tic_array = ["", "", "", "", "", "", "", "", ""]
+    @positions_array = []
   end
 
-  def turn(player, option)
+  def turn(option, position)
+    @tic_array[position] << option
+    @positions_array << position
   end
 
 end
--- spec/tic_spec.rb
+++ spec/tic_spec.rb
@@ -4,10 +4,35 @@ describe TicTacToe do
 
   describe 'Game playing: ' do
     it 'check that class respond to turn method' do
-      player1 = Player.new("Dan")
-      player2 = Player.new("Helen")
+      player1 = Player.new('Dan')
+      player2 = Player.new('Helen')
       game = TicTacToe.new(player1,player2)
       expect(game).to respond_to(:turn).with(2).arguments
     end
+
+    it 'turn method adds the player option in the tic_array' do
+      player1 = Player.new('Dan')
+      player2 = Player.new('Helen')
+      game = TicTacToe.new(player1,player2)
+      game.turn('x', 0)
+      expect(game.tic_array[0]).to eq('x')
+    end
+
+    it 'turn method adds the player position in the positions_array' do
+      player1 = Player.new('Dan')
+      player2 = Player.new('Helen')
+      game = TicTacToe.new(player1,player2)
+      game.turn('x', 0)
+      expect(game.positions_array.length).to eq(1)
+    end
+
+    # it 'turn method reduces number of available fields in a game' do
+    #   player1 = Player.new('Dan')
+    #   player2 = Player.new('Helen')
+    #   game = TicTacToe.new(player1,player2)
+    #   game.turn('x', 0)
+    #   expect(game.tic_array[0]).to eq('x')
+    # end
+
   end
 end
