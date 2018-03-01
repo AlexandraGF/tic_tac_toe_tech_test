5771c49f3c4d7106b3246dd1687f07436a661df3
Alexy <alexandra.fina@yahoo.com>
2018-03-01 11:15:56 +0000

    First spec test to check turn method

---
+++ spec/tic_spec.rb
@@ -0,0 +1,9 @@
+describe Game do
+
+  describe 'Game playing: ' do
+    it 'check that class respond to turn method' do
+      Game.new(player1,player2)
+      expect(game).to respond_to(:turn).with(2).arguments
+    end
+  end
+end 
